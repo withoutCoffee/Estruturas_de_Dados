131bb94ad3f170c50659877c0a8a23903f750221
withoutCoffe <jonasbdc1998@gmail.com>
2019-07-16 21:48:03 -0300

    edit main.c

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "myStack.h"
 
-int main()
-{
-    printf("Hello world!\n");
+/*main utilizada para testes*/
+int main(){
+    myStack pilha;
+
+    iniStack(&pilha,10);/*pasando endereço da pilha e tamanho da mesma*/
+    push(&pilha,50);/*inserindo o número 50*/
+
+    printf("%i",pilha.arr[pilha.top]);
     return 0;
 }
+1;
