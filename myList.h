7cc394b022dcb736e9948bc763efecff4c5f9a56
withoutCoffe <jonasbdc1998@gmail.com>
2019-07-17 09:53:15 -0300

    add mylist.h

---
+++ myList.h
@@ -0,0 +1,22 @@
+#ifndef _MYLIST_
+#define _MYLIST_
+/****
+ * Implementação do módulo: [myList]
+ * Autor: [Jonas Pereira de Oliveira]
+ * Data de Criação: [16/07/19]
+ * Última alteração: [99/99/99]
+ * Descrição Geral: [Modulo com objetivo de estudo sobre estrutura de dados]
+****/
+/*Lista implementada com array*/
+typedef struct(){
+    int *arr;
+    int size;
+    int last;
+
+}myList;
+
+void iniList(myList *l);/*incializa a lista*/
+void append(myList *l,int n);
+void prepend(myList *l,int n);
+
+#endif
