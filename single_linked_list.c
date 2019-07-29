82d289aa0698613ab3cda8c50a6a35d4b645e79f
withoutCoffe <jonasbdc1998@gmail.com>
2019-07-29 11:19:10 -0300

    Add single linked list

--- single_linked_list.c
+++ single_linked_list.c
@@ -4,7 +4,6 @@
 void append_SLList(struct Node** n, int value){
     if(!*n){
         *n = (struct Node*) malloc(sizeof(struct Node));
-
         (*n)->value = value;
         (*n)->next = NULL;
     }else{
@@ -16,8 +15,8 @@ void append_SLList(struct Node** n, int value){
         aux->next->value = value;
         aux->next->next = NULL;
     }
-
 }
+
 void print_list(struct Node** n){
     struct Node *aux = *n;
     while(aux->next){
@@ -26,6 +25,34 @@ void print_list(struct Node** n){
     }
     printf("Value:%i\n",aux->value);
 }
-/*função para inserir no inicio da lista simplesmente encadeada*/
 
+/*função para inserir no inicio da lista simplesmente encadeada*/
+void prepend_SSList(struct Node** n, int value){
+
+    if(!*n){
+        *n = (struct Node*) malloc(sizeof(struct Node));
+        (*n)->value = value;
+        (*n)->next = NULL;
+    }else{
+        struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+        new_node->value = value;
+        new_node->next = *n;
+        *n = new_node;
+    }
+}
 /*função para remover da lista simplesmente encadeada*/
+void remove_SSList(struct Node** n, int index){
+    struct Node *aux = *n, *prev = NULL;
+    int i=0;
+    if(aux!=NULL && index == i){
+        *n = aux->next;
+        return;
+    }
+    while(aux!= NULL && index!= i){
+        ++i;
+        prev = aux;
+        aux = aux->next;
+    }
+    prev->next = aux->next;
+    free(aux);
+}
--- single_linked_list.h
+++ single_linked_list.h
@@ -10,7 +10,8 @@ struct SLList{
     int sizee;
 };
 
-void append_SLList(struct Node** n, int value);/*função para inserr na lista simplesmente encadeada*/
+void append_SLList(struct Node** n, int value);/*função para inserir na lista simplesmente encadeada*/
 void prepend_SSList(struct Node** n, int value);/*função para inserir no inicio da lista simplesmente encadeada*/
 void remove_SSList(struct Node** n, int index);/*função para remover da lista simplesmente encadeada*/
+void print_list(struct Node** n);/*Listar conteúdos dos nó's*/
 #endif
